17d14c85681e2ff66901d19b83322ca2c789286e
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-03-11 17:22:09 +0200

    SubmittedForm and index.css updated

--- Section - 19 - Practice Project/food-order-app/src/components/SubmittedForm.jsx
+++ Section - 19 - Practice Project/food-order-app/src/components/SubmittedForm.jsx
@@ -1,9 +1,9 @@
 export default function SubmittedForm({ onClose }) {
 	return (
-		<div className="control">
+		<div className="control control-center">
 			<h2>Thank you for your order!</h2>
 			<p>Your order has been submitted successfully.</p>
-			<button className="text-button" onClick={onClose}>Close</button>
+			<button className="button" onClick={onClose}>Close</button>
 		</div>
 	);
 }
