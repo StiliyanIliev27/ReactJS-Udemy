451e674052bd6daa69e21b6daf9e303498370f59
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-03-15 22:46:33 +0200

    initialized redux advanced project and added global store to watch for the toggle cart

---
+++ Section - 21 - Advanced Redux/src/components/Cart/CartButton.js
@@ -0,0 +1,21 @@
+import { useDispatch } from 'react-redux';
+
+import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  function toggleCartHandler(){
+    dispatch(uiActions.toggle());
+  }
+
+  return (
+    <button onClick={toggleCartHandler} className={classes.button}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
